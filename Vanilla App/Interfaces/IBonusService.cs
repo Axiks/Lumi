987a167b4ff28480604e98003f9e9df1ed6ebb7c
Yuno <36519646+Axiks@users.noreply.github.com>
2024-10-24 18:33:41 +0200

    make interfaces for bonus system

---
+++ Vanilla App/Interfaces/IBonusService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanilla_App.Models;
+
+namespace Vanilla_App.Interfaces
+{
+    internal interface IBonusService
+    {
+        List<string> GetUsersWithBonus();
+        List<UserBonusModel> GetUserBonuses(string tgId);
+        void TakeBonus(int bonusId);
+    }
+}
---
+++ Vanilla App/Models/UserBonusModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanilla_App.Models
+{
+    internal struct UserBonusModel
+    {
+        public required string TgId { get; init; }
+        public required int BonusId { get; init; }
+        string Title {  get; init; } // Заголовок бонуса
+        string Description {  get; set; } // Опис бонусу
+        DateTime DateOfRegistration { get; init; }  // Коли користувач його вперше зареєстрував
+        DateTime? DateOfUsed { get; init; } // Коли він його використав (Якщо ще ні, то вертається null)
+
+        bool IsUsed { get => DateOfUsed != null ? true : false; }
+
+    }
+}
