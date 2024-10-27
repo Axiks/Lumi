4b09f3cdb210a605816d66452fdf54b7759b03e6
Yuno <36519646+Axiks@users.noreply.github.com>
2024-10-27 12:34:02 +0100

    add poster field

--- Vanilla App/Models/UserBonusModel.cs
+++ Vanilla App/Models/UserBonusModel.cs
@@ -12,7 +12,8 @@ namespace Vanilla_App.Models
         public required int BonusId { get; init; }
         public string ShortTitle { get; init; }
         public string Title {  get; init; } // Заголовок бонуса
-        public string Description {  get; set; } // Опис бонусу
+        public string Description {  get; init; } // Опис бонусу
+        public string CoverUrl { get; init; }
         public DateTime DateOfRegistration { get; set; }  // Коли користувач його вперше зареєстрував
         public DateTime? DateOfUsed { get; set; } // Коли він його використав (Якщо ще ні, то вертається null)
 
