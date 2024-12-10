a0c68a34e92ba5e60fc642b179ae9dcd9764e520
Yuno <36519646+Axiks@users.noreply.github.com>
2024-12-10 21:10:32 +0100

    init bonus api service

--- Vanilla App/Repository/BonusRepository.cs
+++ Vanilla App/Repository/BonusRepository.cs
@@ -1,10 +1,16 @@
 ﻿using Vanilla_App.Interfaces;
 using Vanilla_App.Models;
+using Refit;
 
 namespace Vanilla_App.Repository
 {
     public class BonusRepository : IBonusRepository
     {
+        IBonusApi _api;
+        public BonusRepository()
+        {
+            var _api = RestService.For<IBonusApi>("https://tg-users-api.dan.org.ua");
+        }
         public UserBonusModel GetBonus(long bonusId)
         {
             throw new NotImplementedException();
