63e573cbe6b4448fefd243e3c9810acbb0d91d70
Yuno <36519646+Axiks@users.noreply.github.com>
2024-12-10 21:04:33 +0100

    Added api client Added api model

---
+++ Vanilla App/Interfaces/IBonusApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Refit;
+using Vanilla_App.Models;
+
+namespace Vanilla_App.Interfaces
+{
+    public interface IBonusApi
+    {
+        [Post("/bonuses/{bonus_id}/take")]
+        Task<bool> TakeBonusAsync();
+
+
+        [Get("/bonuses/{bonus_id}")]
+        Task<List<UserBonusModel>> GetBonusAsync(int bonus_id);
+
+
+        [Get("/users/{user_tg_id}/bonuses")]
+        Task<List<UserBonusModel>> GetUserAsync(int user_tg_id);
+
+    }
+}
---
+++ Vanilla App/Repository/BonusRepository.cs
@@ -0,0 +1,28 @@
+﻿using Vanilla_App.Interfaces;
+using Vanilla_App.Models;
+
+namespace Vanilla_App.Repository
+{
+    public class BonusRepository : IBonusRepository
+    {
+        public UserBonusModel GetBonus(long bonusId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UserBonusModel> GetUserBonuses(long tgId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<long> GetUsersWithBonus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeBonus(int bonusId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
