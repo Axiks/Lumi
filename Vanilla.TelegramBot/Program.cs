fc122a63c98a0ad717578967bc54d81ff2a7b5aa
Yuno <36519646+Axiks@users.noreply.github.com>
2024-12-13 18:52:08 +0100

    Added test profile web page

--- Vanilla.TelegramBot/Program.cs
+++ Vanilla.TelegramBot/Program.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Markdig;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using System;
+using System.Text;
+using Telegram.BotAPI.AvailableTypes;
 using Vanilla.Common;
 using Vanilla.OAuth.Services;
 using Vanilla.TelegramBot.Interfaces;
@@ -41,7 +46,7 @@ namespace Vanilla.TelegramBot
 
             // Temp api (remove in future)
             new Thread(delegate () {
-                RunMinimalApi(args);
+                RunMinimalApi(args, serviceProvider);
             }).Start();
 
 
@@ -132,17 +137,82 @@ namespace Vanilla.TelegramBot
         }
 
 
-        async static void RunMinimalApi(string[] args)
+        async static void RunMinimalApi(string[] args, ServiceProvider serviceProvider)
         {
+            var userService = serviceProvider.GetService<IUserService>();
+            var projectsService = serviceProvider.GetService<IProjectService>();
+
+            ConfigurationMeneger confManager = new ConfigurationMeneger();
+            var _settings = confManager.Settings;
+
+            string PrintProfileInformation(Guid userId)
+            {
+                var user = userService.GetUser(userId).Result;
+
+                var userProfile = "![" + user.Username + "](https://" + _settings.Domain + "/storage/" + user.Images.FirstOrDefault().TgMediaId + ".jpg) \r\n\n**" + user.Username + "**\r\n\r\n" + user.About + "\r\n\r\n\r\n";
+
+                var links = String.Format("[{0}](http://t.me/{1})\r\n", user.Username, user.Username);
+                if (user.Links is not null)
+                {
+                    foreach (var link in user.Links)
+                    {
+                        links += String.Format("[{0}]({1})\r\n", link, link);
+                    }
+                }
+                userProfile += links;
+
+                var userProjects = "\n\n---\r\n";
+
+                var projects = projectsService.ProjectGetAllAsync().Result.Where(x => x.OwnerId == user.UserId);
+                if (projects is not null)
+                {
+                    foreach (var project in projects)
+                    {
+                        var projectLinks = "";
+
+                        foreach (var projectLink in project.Links)
+                        {
+                            projectLinks += String.Format("[{0}]({1})\r\n", projectLink, projectLink);
+                        }
+
+                        userProjects += String.Format("**{0}**\r\n*{1}*\r\n\r\n{2}\r\n\r\n{3}\r\n\r\n---\n\n", project.Name, project.DevelopmentStatus.ToString(), project.Description, projectLinks);
+                    }
+                }
+
+                userProfile += userProjects;
+
+                return Markdown.ToHtml(userProfile);
+            }
+
+
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
+
+
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "storage")),
                 RequestPath = "/storage"
             });
             app.MapGet("/", () => "Hello World!");
-            app.MapGet("/test", () => "It`s test");
+            app.MapGet("/health", () => "online");
+
+            app.MapGet("/user/{userId}", 
+                async context =>
+            {
+                context.Response.ContentType = "text/html; charset=UTF8";
+
+                var someValueFromGet = context.Response;
+
+                if (context.Request.RouteValues.ContainsKey("userId"))
+                {
+                    var result = PrintProfileInformation(Guid.Parse(context.Request.RouteValues["userId"].ToString()));
+                    await context.Response.WriteAsync(result, Encoding.UTF8);
+                }
+
+            });
+
+
             app.Run();
         }
 
